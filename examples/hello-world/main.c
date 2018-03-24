6cc50b696d9af45e8b931d321a7195b668cac697
sashaindri <alexrover2020@yandex.ru>
2018-03-24 17:39:08 +0300

    v.02

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,13 +20,38 @@
  */
 
 #include <stdio.h>
+#include "periph/gpio.h"
+#include "rtctimers-millis.h"
+
+
+uint32_t millisecond_last_press=0;
+
+void btn_tolse(void *arg){
+	(void)arg;
+	
+	
+	if((rtctimers_millis_now()-millisecond_last_press)>100){
+		gpio_toggle(GPIO_PIN(PORT_B,0));
+		millisecond_last_press = rtctimers_millis_now();
+		
+	}
+	
+}
 
 int main(void)
 {
+	rtctimers_millis_init();
+	//rtctimers_millis_init();
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+	
+	gpio_init(GPIO_PIN(PORT_B,0),GPIO_OUT);
+	gpio_set(GPIO_PIN(PORT_B,0));
+	
+	gpio_init_int(GPIO_PIN(PORT_B,1),GPIO_IN_PU,GPIO_FALLING,btn_tolse,NULL);
+	
 
     return 0;
 }
